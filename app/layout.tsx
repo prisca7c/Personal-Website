89b1e3517f9fa3e1c01322bc0592f514f86be1ef
Prisca Chien <131054201+prisca7c@users.noreply.github.com>
2025-08-03 20:05:01 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Prisca Chien',
+  description: 'Created by Prisca Chien',
+  generator: 'priscachien.com',
 }
 
 export default function RootLayout({
